3151995064430347f52df4c755e06a5a21141d4a
Vinicius.Araujo <vinicius.araujo@ifpi.edu.br>
2012-09-03 18:41:13 -0300

    Alterado formato do enunciados dos requisitos Para o padrão O Projeto devera permitir que o sujeito ....

--- Planejando/Requisitos Funcionais    Desejaveis.mm
+++ Planejando/Requisitos Funcionais    Desejaveis.mm
@@ -13,14 +13,13 @@
       Desejaveis
     </p>
   </body>
-</html>
-</richcontent>
-<node CREATED="1346365423986" ID="ID_1286235818" MODIFIED="1346365943535" POSITION="right" TEXT="Projeto de Coaching deve ser visto como um programa&#xa; de treinamento em Gest&#xe3;o de Projetos e Portifolio de Projetos"/>
-<node CREATED="1346365434961" ID="ID_1956722582" MODIFIED="1346365923983" POSITION="left" STYLE="fork" TEXT="Todo o treinamento de Gerencia de Configura&#xe7;&#xe3;o &#xa;deve ser visto como um programa de treinamento &#xa;e deve possuir plano de continuidade">
+</html></richcontent>
+<node CREATED="1346365423986" HGAP="19" ID="ID_1286235818" MODIFIED="1346707456862" POSITION="right" TEXT="Projeto deve permitir que o subprojeto de Coaching deve ser visto como&#xa; um programa  de treinamento em Gest&#xe3;o de Projetos e&#xa; Portifolio de Projetos para garantir a &#xa;continuidade da cultura em gest&#xe3;o de projetos" VSHIFT="-19"/>
+<node CREATED="1346365434961" HGAP="16" ID="ID_1956722582" MODIFIED="1346707675878" POSITION="left" STYLE="fork" TEXT="O projeto deve permitir  a equipe realize o planejamento do subprojeto de&#xa;treinamento em  Gerencia de Configura&#xe7;&#xe3;o visando sua transforma&#xe7;&#xe3;o em&#xa;um Programa de Treinamentos &#xa;em Gerencia de Configura&#xe7;&#xe3;o&#xa;e deve possuir plano de continuidade" VSHIFT="-26">
 <edge STYLE="bezier"/>
 </node>
-<node CREATED="1346366024114" ID="ID_1888601467" MODIFIED="1346366256967" POSITION="right" TEXT="o plano  implanta&#xe7;&#xe3;o de Metodologia de Gest&#xe3;o de Projeto&#xa; deve visar as boas praticas praticadas no governo, &#xa;principalmente no governo federal"/>
-<node CREATED="1346366324249" ID="ID_1203249682" MODIFIED="1346367121678" POSITION="left" TEXT="projeto deve fazer pesquisa de ferramentas &#xa;usadas para aplica&#xe7;&#xe3;o  da metodologia de gest&#xe3;o de projetos&#xa; usadas nos org&#xe3;os do governo, principalmento do governo federal"/>
-<node CREATED="1346367039913" ID="ID_1072820296" MODIFIED="1346367056136" POSITION="right" TEXT="O projeto dever&#xe1; seguir a terminologia do PMI"/>
+<node CREATED="1346366024114" ID="ID_1888601467" MODIFIED="1346707434510" POSITION="right" TEXT="O projeto dever permitir que o plano de  implanta&#xe7;&#xe3;o de &#xa;Metodologia de Gest&#xe3;o de Projeto&#xa; contemple as boas praticas praticadas no governo, &#xa;principalmente no governo federal"/>
+<node CREATED="1346366324249" HGAP="21" ID="ID_1203249682" MODIFIED="1346707569542" POSITION="left" TEXT="Projeto deve  permitir que a equipe ou parte dela fa&#xe7;a  pesquisa de ferramentas &#xa;usadas para aplica&#xe7;&#xe3;o  da metodologia de gest&#xe3;o de projetos&#xa; usadas nos org&#xe3;os do governo, principalmente do governo federal" VSHIFT="-3"/>
+<node CREATED="1346367039913" HGAP="33" ID="ID_1072820296" MODIFIED="1346707262262" POSITION="right" TEXT="O projeto dever&#xe1; seguir a terminologia do PMI" VSHIFT="25"/>
 </node>
 </map>
