b2a460dd28e2b47a0d7d1fe3d6e562d41b5e289f
Vinicius.Araujo <vinicius.araujo@ifpi.edu.br>
2012-09-03 18:01:04 -0300

    Adcionados requisitos funcionais e wbs e Plano de gestão de escopo

---
+++ Planejando/Requisitos Funcionais    Desejaveis.mm
@@ -0,0 +1,26 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#00cc00" CREATED="1346363778980" ID="ID_744638019" MODIFIED="1346367104808">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Requisitos Funcionais
+    </p>
+    <p>
+      Desejaveis
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1346365423986" ID="ID_1286235818" MODIFIED="1346365943535" POSITION="right" TEXT="Projeto de Coaching deve ser visto como um programa&#xa; de treinamento em Gest&#xe3;o de Projetos e Portifolio de Projetos"/>
+<node CREATED="1346365434961" ID="ID_1956722582" MODIFIED="1346365923983" POSITION="left" STYLE="fork" TEXT="Todo o treinamento de Gerencia de Configura&#xe7;&#xe3;o &#xa;deve ser visto como um programa de treinamento &#xa;e deve possuir plano de continuidade">
+<edge STYLE="bezier"/>
+</node>
+<node CREATED="1346366024114" ID="ID_1888601467" MODIFIED="1346366256967" POSITION="right" TEXT="o plano  implanta&#xe7;&#xe3;o de Metodologia de Gest&#xe3;o de Projeto&#xa; deve visar as boas praticas praticadas no governo, &#xa;principalmente no governo federal"/>
+<node CREATED="1346366324249" ID="ID_1203249682" MODIFIED="1346367121678" POSITION="left" TEXT="projeto deve fazer pesquisa de ferramentas &#xa;usadas para aplica&#xe7;&#xe3;o  da metodologia de gest&#xe3;o de projetos&#xa; usadas nos org&#xe3;os do governo, principalmento do governo federal"/>
+<node CREATED="1346367039913" ID="ID_1072820296" MODIFIED="1346367056136" POSITION="right" TEXT="O projeto dever&#xe1; seguir a terminologia do PMI"/>
+</node>
+</map>
